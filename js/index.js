6a4a952632821eb7ec76843948041eb17711a91c
anacnnr <anacnrezende@gmail.com>
2025-02-19 18:06:06 -0300

    Adição da lógica inicial  para pesquisa dos desenhos

--- js/index.js
+++ js/index.js
@@ -1,6 +1,23 @@
 document.addEventListener("DOMContentLoaded", ()=>{
+console.log("carregado");
 
-   
-    
-    //Evento da seta direita
+const sectionElements = document.querySelectorAll("section")
+
+//CAPTURAR O VALUE DO INPUT E MANIPULAR A LÓGICA
+
+sectionElements.forEach(iten => {
+let itenClass = iten.className
+
+if(itenClass == "year_2022"){
+    //console.log("Desenhos de 2022");
+}
+else{
+    //Classe que não é de 2022 desaparece
+    iten.style.display = 'none'  
+    //Capturar o h3 que estiver perto dessas sections
+    const titleDraw = iten.previousElementSibling
+    titleDraw.style.display = 'none'
+}
+});
+
 });//Carregamento da pág
